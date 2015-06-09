0c7b329caf952a4fe8eab3f758fd1fc8abd3252c
Netix (Espinet François) <dev.espinetfrancois@gmail.com>
2015-06-09 14:55:54 +0200

    Added zip storage

--- sims/storage.py
+++ sims/storage.py
@@ -5,7 +5,13 @@ import numpy as np
 import pandas as pd
 import os
 
+lockfile = os.path.join(os.path.dirname(os.path.realpath(__file__)), ".db_lock")
+
 class Storage(object):
+    def get_data(self):
+        pass
+
+class ProcessedStorage(Storage):
     def __enter__(self):
         return self
 
@@ -18,29 +24,144 @@ class Storage(object):
     def write_row(self, rowdict):
         pass
 
-    def get_data(self):
+    def flush(self):
         pass
 
-class PandasPickle(Storage):
+
+class RawStorage(Storage):
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
+
+    def prepare(self):
+        pass
+
+    def write_row(self, rowdict):
+        pass
+
+    def flush(self):
+        pass
+
+import zipfile
+import tempfile
+import shutil
+import json
+
+
+class Zip(RawStorage):
+
+    CONFIG = "conf.json"
 
     def __init__(self, database):
+        self.database = database + ".zip"
+        self.zip = None
+        self.data = None
+        self.rowlist = []
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
+
+    def prepare(self):
+        self.zip = zipfile.ZipFile(self.database, 'a', zipfile.ZIP_DEFLATED)
+
+    def store(self, output_dir, conf, root):
+        for key, value in conf.iteritems():
+            conf[key] = os.path.relpath(value, output_dir)
+        with open(os.path.join(output_dir, self.CONFIG), 'w') as f:
+            json.dump(conf, f)
+
+        for root_dir, dirs, files in os.walk(output_dir):
+            for fi in files:
+                path = os.path.join(root_dir, fi)
+                self.zip.write(path, os.path.relpath(path, root))
+        # self.zip.write(output_dir, os.path.relpath(output_dir, root))
+
+    def get_data(self):
+        if self.data is None:
+            self.data = pd.DataFrame(self.rowlist).convert_objects(convert_dates=True, convert_numeric=True, convert_timedeltas=True)
+        return self.data
+
+    def get_results(self):
+        if self.zip is None:
+            self.zip = zipfile.ZipFile(self.database, 'r', zipfile.ZIP_DEFLATED)
+        extract_dir = tempfile.mkdtemp()
+        print "Writing to %s" % extract_dir
+        self.zip.extractall(extract_dir)
+        # results = []
+        for output_dir, dirs, files in os.walk(extract_dir):
+            if output_dir == extract_dir:
+                continue
+            with open(os.path.join(output_dir, self.CONFIG), 'r') as f:
+                conf = json.load(f)
+            for key, value in conf.iteritems():
+                conf[key] = os.path.join(output_dir, value)
+            yield (output_dir, conf)
+
+        shutil.rmtree(extract_dir)
+
+    def write_row(self, rowdict):
+        self.rowlist.append(rowdict)
+
+
+    def flush(self):
+        with FileLock(lockfile) as lock:
+            self.zip.close()
+
+
+class PandasHDF(ProcessedStorage):
+    def __init__(self, database):
         self.data = pd.DataFrame()
-        self.database = database
+        self.database = database + ".hd5"
         self.rowlist = []
         self.convert = None
 
     def __enter__(self):
-        if os.path.exists(self.database):
-            self.data = pd.read_pickle(self.database)
+
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        newdata = pd.DataFrame(self.rowlist)
-        d = pd.concat([self.data, newdata])
-        d.to_pickle(self.database)
+        pass
 
     def prepare(self):
         pass
+
+    def write_row(self, rowdict):
+        self.rowlist.append(rowdict)
+
+    def get_data(self):
+        if self.data is None:
+            if os.path.exists(self.database):
+                self.data = pd.read_hdf(self.database, "dt")
+        return self.data
+
+    def flush(self):
+        with FileLock(lockfile) as lock:
+            newdata = pd.DataFrame(self.rowlist)
+            newdata.to_hdf(self.database, "dt", format = 't', append = True)
+
+class PandasJson(ProcessedStorage):
+
+    def __init__(self, database):
+        self.data = pd.DataFrame()
+        self.database = database+".json"
+        self.rowlist = []
+        self.convert = None
+
+    def __enter__(self):
+
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
+
+    def prepare(self):
+        if os.path.exists(self.database):
+            self.data = pd.read_json(self.database, orient = 'split')
 
     def write_row(self, rowdict):
         self.rowlist.append(rowdict)
@@ -50,59 +171,143 @@ class PandasPickle(Storage):
             self.convert = self.data.convert_objects(convert_dates=True, convert_numeric=True, convert_timedeltas=True)
         return self.convert
 
-class PandasCsv(Storage):
+    def flush(self):
+        with FileLock(lockfile) as lock:
+            newdata = pd.DataFrame(self.rowlist)
+            d = pd.concat([self.data, newdata])
+            d.to_json(self.database, orient = 'split')
+
+class PandasPickle(ProcessedStorage):
 
     def __init__(self, database):
         self.data = pd.DataFrame()
-        self.database = database
+        self.database = database+".pickle"
         self.rowlist = []
+        self.convert = None
 
     def __enter__(self):
-        if os.path.exists(self.database):
-            self.data = pd.read_csv(self.database)
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        newdata = pd.DataFrame(self.rowlist)
-        d = pd.concat([self.data, newdata])
-        d.to_csv(self.database, index=False)
+        pass
 
     def prepare(self):
-        pass
+        if os.path.exists(self.database):
+            self.data = pd.read_pickle(self.database)
 
     def write_row(self, rowdict):
         self.rowlist.append(rowdict)
 
     def get_data(self):
-        return self.data
-### Sqlite implementation ###
+        if self.convert is None:
+            self.convert = self.data.convert_objects(convert_dates=True, convert_numeric=True, convert_timedeltas=True)
+        return self.convert
 
+    def flush(self):
+        with FileLock(lockfile) as lock:
+            newdata = pd.DataFrame(self.rowlist)
+            d = pd.concat([self.data, newdata])
+            d.to_pickle(self.database)
+
+from csv import QUOTE_ALL
+from StringIO import StringIO
+
+import cPickle
+
+class PandasCsv(ProcessedStorage):
+
+    ARRAY_TYPE = "np2darray"
+
+    def __init__(self, database):
+        self.data = None
+        self.types = None
+        self.database = database+".csv"
+        self.rowlist = []
+        self.create = False
+
+    def __enter__(self):
+        if os.path.exists(self.database):
+            self.types = pd.read_csv(self.database, nrows = 1).iloc[0].to_dict()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
+
+    def prepare(self):
+        self.create = not os.path.exists(self.database)
+
+    def flush(self):
+        with FileLock(lockfile) as lock:
+            if not self.create:
+                newdata = pd.DataFrame(self.rowlist)
+            else:
+                newdata = pd.DataFrame([self.types]+self.rowlist)
+            newdata.to_csv(self.database, mode = 'a', index = False, header = self.create, quoting = QUOTE_ALL, line_terminator=";")
+
+    def write_row(self, rowdict):
+        if self.types is None:
+            self.types = pd.DataFrame([rowdict]).dtypes.to_dict()
+            for key, val in rowdict.iteritems():
+                if type(val) is np.ndarray:
+                    self.types[key] = self.ARRAY_TYPE
+
+        for key, val in rowdict.iteritems():
+            if type(val) is np.ndarray:
+                rowdict[key] = self.dump_array(val)
+
+        self.rowlist.append(rowdict)
+
+    def get_data(self):
+        if self.data is None:
+            if os.path.exists(self.database):
+                self.data = pd.read_csv(self.database, skiprows=[1], lineterminator=";")
+                self.data.apply(self.pump_arrays, axis = 1)
+
+        return self.data
+
+    def pump_arrays(self, row):
+        for key, val in self.types.iteritems():
+            if val == self.ARRAY_TYPE:
+                row[key] = self.load_array(row[key])
+        return row
+
+    def load_array(self, arr):
+        return cPickle.loads(arr)
+        # return np.loadtxt(StringIO(arr), delimiter="|")
+
+    def dump_array(self, arr):
+        return cPickle.dumps(arr)
+        # out = StringIO()
+        # np.savetxt(out, arr, delimiter="|")
+        # return out.getvalue()
+
+### Sqlite implementation ###
 class TypeHelper(object):
-    SQLITE_TXT = "TEXT"
-    SQLITE_INT = "INT"
-    SQLITE_FLOAT = "REAL"
-    SQLITE_BLOB = "BLOB"
-    CUSTOM_ARRAY = "ARRAY"
+    TXT = "TEXT"
+    INT = "INT"
+    FLOAT = "REAL"
+    BLOB = "BLOB"
+    ARRAY = "ARRAY"
 
     @classmethod
     def getType(cls, sample):
         if type(sample) is np.ndarray:
-            return cls.CUSTOM_ARRAY
+            return cls.ARRAY
         elif type(sample) in [basestring, str]:
             return cls.getTypeFromString(sample)
         elif type(sample) in (float, np.float_):
-            return cls.SQLITE_FLOAT
+            return cls.FLOAT
         elif type(sample) is int:
-            return cls.SQLITE_INT
+            return cls.INT
         elif type(sample) is bool:
-            return cls.SQLITE_TXT
-        return cls.SQLITE_BLOB
+            return cls.TXT
+        return cls.BLOB
 
     @classmethod
     def getTypeFromString(cls, s):
         try:
             float(s)
-            return cls.SQLITE_FLOAT
+            return cls.FLOAT
         except ValueError:
             pass
 
@@ -111,13 +316,13 @@ class TypeHelper(object):
 
             d = unicodedata.numeric(s)
             if type(d) == float:
-                return cls.SQLITE_FLOAT
+                return cls.FLOAT
             elif type(d) == int:
-                return cls.SQLITE_INT
+                return cls.INT
         except (TypeError, ValueError):
             pass
 
-        return cls.SQLITE_TXT
+        return cls.TXT
 
     @staticmethod
     def is_number(s):
@@ -152,7 +357,7 @@ def convert_array(text):
     out.seek(0)
     return np.load(out)
 
-class Sqlite3(Storage):
+class Sqlite3(ProcessedStorage):
     table = 'experiments'
 
     def get_data(self):
@@ -163,7 +368,7 @@ class Sqlite3(Storage):
         sqlite3.register_adapter(np.ndarray, adapt_array)
 
         # Converts TEXT to np.array when selecting
-        sqlite3.register_converter(TypeHelper.CUSTOM_ARRAY, convert_array)
+        sqlite3.register_converter(TypeHelper.ARRAY, convert_array)
         self.connection = sqlite3.connect(database, detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES)
 
     def __enter__(self):
@@ -239,7 +444,6 @@ class Sqlite3(Storage):
 
 
 import fcntl
-
 
 class FileLock(object):
     def __init__(self, file):
